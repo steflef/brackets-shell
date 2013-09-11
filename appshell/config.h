d7f8b6f9aff2f2e4f6ec1e68b6bebab9d88011a9
steflef <prof.lebrun@gmail.com>
2013-09-11 15:05:36 -0400

    changes to the name build

--- appshell/config.h
+++ appshell/config.h
@@ -41,7 +41,7 @@
 // Name of group (if any) that application prefs/settings/etc. are stored under
 // This must be an empty string (for no group), or a string that ends with "/"
 #define GROUP_NAME @""
-#define APP_NAME @"Brackets"
+#define APP_NAME @"[STEFLEF]"
 #define WINDOW_TITLE APP_NAME
 
 // Paths for node resources are relative to the bundle path
